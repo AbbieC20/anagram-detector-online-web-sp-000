a1e9ef96ffab5150019932e73e4442264a3b37c8
remote-torrent-6747 <abbyc@sky.com>
2021-01-18 18:58:14 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -17,13 +17,3 @@ class Anagram
   end
 
 end
-
-
-
-#should compare word passed in, against an array
-#should return any words in the array that are anagram
-
-# should detect no matches
-# should detect a simple anagram
-# should detect an anagram
-# should detect multiple anagrams
